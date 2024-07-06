ab8ca85e46d5fda170e124c946e892b7c242d945
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2024-07-06 22:51:31 +0100

    feat: got text grid and movement working

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,38 @@
-import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
+import { Boot } from "./scenes/Boot";
+import { Game as MainGame } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import { Preloader } from "./scenes/Preloader";
 
 import { Game, Types } from "phaser";
+
+const graphicsSettings = { best: 1, medium: 0.75, low: 0.5 };
+const DPR = window.devicePixelRatio * graphicsSettings.best;
+const { width, height } = window.screen;
+
+// Set width and height.
+const WIDTH = Math.round(Math.max(width, height) * DPR);
+const HEIGHT = Math.round(Math.min(width, height) * DPR);
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  type: Phaser.AUTO,
+  width: WIDTH,
+  height: HEIGHT,
+  parent: "game-container",
+  backgroundColor: "#028af8",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  input: {
+    keyboard: true,
+  },
+  // zoom: 4,
+  // pixelArt: true,
+  // roundPixels: true,
 };
 
 export default new Game(config);
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,102 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+interface KeyMap {
+  h: Phaser.Input.Keyboard.Key;
+  j: Phaser.Input.Keyboard.Key;
+  k: Phaser.Input.Keyboard.Key;
+  l: Phaser.Input.Keyboard.Key;
+}
 
-    constructor ()
-    {
-        super('Game');
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+  msg_text: Phaser.GameObjects.Text;
+  private gridWidth: number;
+  private gridHeight: number;
+  private cellSize: number;
+  private cellWidth: number;
+  private fontSize: string;
+  private grid: Phaser.GameObjects.Text[];
+  private selectedCell: { x: number; y: number };
+  private keys: KeyMap;
+
+  constructor() {
+    super("Game");
+
+    // get the game window size
+    this.cellSize = 48;
+    this.cellWidth = 26;
+    this.fontSize = `${this.cellSize - 6}px`;
+    this.grid = [];
+    this.selectedCell = { x: 0, y: 0 };
+  }
+
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x000000);
+
+    // Initialize the grid with random characters
+    this.gridWidth = Math.floor(+this.game.config.width / this.cellWidth);
+    this.gridHeight = Math.floor(+this.game.config.height / this.cellSize);
+
+    for (let y = 0; y < this.gridHeight; y++) {
+      for (let x = 0; x < this.gridWidth; x++) {
+        const char = String.fromCharCode(33 + Math.floor(Math.random() * 94)); // ASCII 33-126
+        // box characters
+        // const char = String.fromCharCode(
+        //   0x2500 + Math.floor(Math.random() * 96),
+        // );
+        // const char = String.fromCharCode(0x2588); // █
+        const text = this.add.text(
+          x * this.cellWidth,
+          y * this.cellSize,
+          char,
+          {
+            fontFamily: "Input Mono",
+            fontSize: this.fontSize,
+            color: "rgb(255,255,255)",
+            fixedWidth: this.cellWidth,
+          },
+        );
+        this.grid.push(text);
+      }
     }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+    this.keys = this.input.keyboard!.addKeys({
+      h: Phaser.Input.Keyboard.KeyCodes.H,
+      j: Phaser.Input.Keyboard.KeyCodes.J,
+      k: Phaser.Input.Keyboard.KeyCodes.K,
+      l: Phaser.Input.Keyboard.KeyCodes.L,
+    }) as KeyMap;
+  }
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+  update() {
+    const prevText =
+      this.grid[this.selectedCell.y * this.gridWidth + this.selectedCell.x];
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
+    // Check for key presses
+    if (this.keys.h.isDown) {
+      this.selectedCell.x = Math.max(0, this.selectedCell.x - 1);
+    } else if (this.keys.j.isDown) {
+      this.selectedCell.y = Math.min(
+        this.gridHeight - 1,
+        this.selectedCell.y + 1,
+      );
+    } else if (this.keys.k.isDown) {
+      this.selectedCell.y = Math.max(0, this.selectedCell.y - 1);
+    } else if (this.keys.l.isDown) {
+      this.selectedCell.x = Math.min(
+        this.gridWidth - 1,
+        this.selectedCell.x + 1,
+      );
     }
+
+    const text =
+      this.grid[this.selectedCell.y * this.gridWidth + this.selectedCell.x];
+    prevText.setBackgroundColor("rgb(0,0,0)");
+    prevText.setColor("rgb(255,255,255)");
+
+    text.setBackgroundColor("rgb(255,255,255)");
+    text.setColor("rgb(0,0,0)");
+  }
 }
